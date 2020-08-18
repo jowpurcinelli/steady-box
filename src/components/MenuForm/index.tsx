b3d557a88dd84418bd1ef193e5cd9f3b0d68f5ff
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-08-18 20:40:36 -0300

    structuringApp

---
+++ src/components/MenuForm/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { 
+    Container, 
+    Navigation, 
+    DropboxLogo, 
+    Form } from './styles';
+
+const MenuForm: React.FC = ( ) => {
+    function handleToggle( ) {
+        if (window.toggleActiveMenu)
+        window.toggleActiveMenu( );
+    }
+
+    return (
+        <Container>
+            <Navigation>
+                <h1>
+                    <DropboxLogo />
+                    <span>Dropbox</span>
+                </h1>
+                <button
+                className="ation--close"
+                onClick={handleToggle}>
+                    X
+                </button>
+            </Navigation>
+        </Container>
+    )
+
+}
