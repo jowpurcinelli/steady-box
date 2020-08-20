651d48f0c1bf6155bcb04718339aa04f8ef4257c
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-08-20 00:05:04 -0300

    dale

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,1 +1,14 @@
 import React from 'react';
+
+import { CompleteForm} from '../Autocomplete';
+
+const CompleteForm( ) {
+    return(
+    <div
+    className="Sidemenu">
+        <CompleteForm/>
+
+
+    </div>
+    )
+}
