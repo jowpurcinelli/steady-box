6a1220709cac92b444b10eb9b507799cae9a4971
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-09-08 10:25:28 -0300

    sideMenu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 
-import { CompleteForm} from '../Autocomplete';
+//import { CompleteForm} from '../Autocomplete';
 
-const completeFormElement (  {
-    return(
-    <div
-    className="Sidemenu">
-        <CompleteForm/>
-
-
-    </div>
-    )
-)
+declare Global {
+    interface Window {
+        toggleActiveMenu:   (() => void  ) | undefined;
+    }
 }
 
-export default
+const scrollThreshold = 300;
+
+const SideMenu : React.FC = ({ children }) => {
+    const [isActive, setIsActive] = useState(false);
+    const [ scrollY, setScrollY ] = useState(0);
+
+    useEffect(() => {
+        function onScroll ( ) {
+            setScrollY( windows.scrollY );
+            setIsActive (false);
+        }
+
+        window.addEventListener('scroll', onScroll);
+        return ( ) => window.removeEventListener('scroll', onScroll);
+    },
+        [scrollY]);
+
+
+ 
+}
+
+export default SideMenu;
+
